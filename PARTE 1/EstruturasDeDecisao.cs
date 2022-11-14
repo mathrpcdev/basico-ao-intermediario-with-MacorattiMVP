8d84f2606758d40b43147a224db867a773514754
Matheus Rodrigues Cavalcante <96153654+mathrpcdev@users.noreply.github.com>
2022-11-14 13:46:58 -0300

    First commit

---
+++ PARTE 1/EstruturasDeDecisao.cs
@@ -0,0 +1,30 @@
+﻿bool valor = true;
+
+if (valor == true)
+{
+    Console.WriteLine(" O valor é {0}", valor);
+}
+
+int a,
+    b;
+a = 7;
+b = 7;
+
+if (a < b)
+{
+    Console.WriteLine("A é menor que B");
+    Console.WriteLine("O valor de A é: {0}", a);
+    Console.WriteLine("O valor de B é: {0}", b);
+}
+else if (a > b)
+{
+    Console.WriteLine($"A é maior que B, que vale: {0}", b);
+    Console.WriteLine("O valor de A é: {0}", a);
+    Console.WriteLine("O valor de B é: {0}", b);
+}
+else
+{
+    Console.WriteLine("A é igual a B: {0}", b);
+    Console.WriteLine("O valor de A é: {0}", a);
+    Console.WriteLine("O valor de B é: {0}", b);
+}
