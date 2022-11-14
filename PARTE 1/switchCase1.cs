3a47aa6a00b59a246b28d25b7a389d279306d96d
Matheus Rodrigues Cavalcante <96153654+mathrpcdev@users.noreply.github.com>
2022-11-14 18:11:43 -0300

    First commit

---
+++ PARTE 1/switchCase1.cs
@@ -0,0 +1,39 @@
+﻿/*
+Author: Matheus Cavalcante
+Exercício: Controle de Fluxo.
+ */
+
+string carro;
+string fabricante;
+
+Console.WriteLine("Por favor, nos diga a marca de seu carro.");
+carro = Console.ReadLine();
+
+switch (carro)
+{
+    case "Punto":
+    case "Uno":
+    case "Palio":
+        fabricante = "Fiat";
+        break;
+    case "Spin":
+    case "Tracker":
+    case "Camaro":
+        fabricante = "Chevrolet";
+        break;
+    case "Frontier":
+    case "Kiks":
+    case "Versa":
+        fabricante = "Nissan";
+        break;
+    case "Civic":
+    case "Fit":
+    case "City":
+        fabricante = "Honda";
+        break;
+    default:
+        fabricante = "Desconhecido.";
+        break;
+}
+
+Console.WriteLine($"De acordo com a marca de seu carro, a fabricante dele é {fabricante}");
