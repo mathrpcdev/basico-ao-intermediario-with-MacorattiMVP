9257b54bbd2a3afedf2f0512d8aa52c0b4d68408
Matheus Rodrigues Cavalcante <96153654+mathrpcdev@users.noreply.github.com>
2022-11-15 09:27:58 -0300

    First commit

---
+++ PARTE 1/EstruturasDeRepeticaoDoEDoWhile.cs
@@ -0,0 +1,31 @@
+﻿/*
+Author: Matheus Cavalcante
+Exercício: Estruturas de repetição  do while e while.
+ */
+
+// A verificação no ' do while ' é feita após a execução, diferentemente do ' while '.
+
+int j = 10;
+do
+{
+    Console.WriteLine($"Numero {j}");
+    j--;
+} while (j > 0);
+
+// Aqui a verificação é feita antes.
+int i = 0;
+
+while (i < 10)
+{
+    Console.WriteLine($"i = {i}");
+    i++;
+}
+
+// Essa estrutura de repetição não é uma boa prática e foi herdada do Visual Basic.
+// goto não deve mais ser usado!
+int contador = 0;
+repeat: // <-- Isso é chamado de Label.
+Console.WriteLine($"Contador é igual a = {contador}");
+contador++;
+if (contador < 10)
+    goto repeat;
